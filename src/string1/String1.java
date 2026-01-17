b0f4ee42c237732fe1fa978408b96865467f7c5c
Aynishah <aynishah10@gmail.com>
2026-01-17 18:53:29 +0600

    makeOutWord

--- src/string1/String1.java
+++ src/string1/String1.java
@@ -17,5 +17,11 @@ public class String1 {
         return "<" + tag + ">" + word + "<" + "/" + tag + ">";
     }
 
+    public String makeOutWord(String out, String word) {
+        return out.substring(0,2) + word + out.substring(2,4);
+    }
+
+
+
 
 }
