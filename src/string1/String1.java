1bb93923e5c1c48fc3da0b4749f9deb500be129b
Aynishah <aynishah10@gmail.com>
2026-01-17 18:49:51 +0600

    helloName

---
+++ src/string1/String1.java
@@ -0,0 +1,13 @@
+package string1;
+
+public class String1 {
+    public static void main(String[] args) {
+
+    }
+
+    public String helloName(String name) {
+        return "Hello " + name +"!";
+    }
+
+
+}
