96517d144244f4db03468800c1ba17da9e5abe4e
Aynishah <aynishah10@gmail.com>
2026-01-17 19:00:16 +0600

    right2

--- src/string1/String1.java
+++ src/string1/String1.java
@@ -68,6 +68,12 @@ public class String1 {
         }
     }
 
+    public String right2(String str) {
+        String end = str.substring(str.length()-2);
+        String start = str.substring(0,str.length()-2);
+        return end + start;
+    }
+
 
 
 
