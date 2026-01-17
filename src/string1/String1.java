ebedba0c459eb806cb3b1aa8a9658f594171d771
Aynishah <aynishah10@gmail.com>
2026-01-17 19:11:55 +0600

    hasBad

--- src/string1/String1.java
+++ src/string1/String1.java
@@ -127,6 +127,23 @@ public class String1 {
         );
     }
 
+    public boolean hasBad(String str) {
+        if (str.length()< 3 ){
+            return false;
+        }
+        if (str.length() >= 3 && str.substring(0,3).equals("bad")){
+            return true;
+        }
+
+        if (str.length() >= 4 && str.substring(1,4).equals("bad")){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+
+
 
 
 
